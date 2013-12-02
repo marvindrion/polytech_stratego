bd9be28d9c3b875ba63188eeb1ebc23056356d3a
Antoine Chalifour <antoine.chalifour@gmail.com>
2013-12-02 19:20:28 +0100

    Ajout de la liste des pions découverts de l'adversaire.

--- Projet_1/Stratego/Interface.c
+++ Projet_1/Stratego/Interface.c
@@ -10,6 +10,8 @@ typedef struct Pion{
 typedef struct Joueur{
 	int nombreDeFautes; //init à 0
 	int camp ; // blue ou rouge, pareil que les pions donc utiliser une enum;
+	Pion* decouverts[40];
+    int nbDecouverts; // pour manipuler le tableau
 }Joueur ;
 
 typedef Pion* Terrain[10][10]; // chaque case contient un pointeur vers un pion, case vide = NULL
