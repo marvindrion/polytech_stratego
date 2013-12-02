9020b0e6c15cc466507e418136a1dc2681d83830
Antoine Chalifour <antoine.chalifour@gmail.com>
2013-12-02 18:36:36 +0100

    Ajout des structures / typedef

---
+++ Interface.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Pion{
+	int force;
+	int camp; //bleu ou rouge
+	int erreur; //pour gerer la regle des 2case(voir regle 2CASES
+} Pion;
+
+typedef struct Joueur{
+	int nombreDeFautes; //init à 0
+	int camp ; // blue ou rouge, pareil que les pions donc utiliser une enum;
+}Joueur ;
+
+typedef Terrain *Pion[10][10] ; // chaque case contient un pointeur vers un pion, case vide = NULL
